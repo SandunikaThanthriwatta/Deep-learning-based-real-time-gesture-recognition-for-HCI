86b8db3a92141229c142a7ffee48b1e716887b62
Sandunika Thanthriwatta <s.thanthriwatta07@gmail.com>
2024-02-20 19:20:53 +0530

    Implement gesture prediction model binding

---
+++ 2_Application_Actions/Custom/Load_gesture_map.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+"""
+Function to load gesture_map from JSON file
+"""
+def load_gesture_map(file_path):
+    with open(file_path, 'r') as f:
+        custom_config = json.load(f)
+    print("Loaded custom configurations: ", custom_config)
+    return custom_config
+
+
+
+
+"""
+# Example usage: Load gesture_map from a file
+folder_path = r'C:\\Users\M\Desktop\TestAppName'
+os.makedirs(folder_path, exist_ok=True)  # Create folder if it doesn't exist
+file_path = os.path.join(folder_path, 'custom_gesture_map.json')
+
+loaded_custom_gesture_map = load_gesture_map(file_path)
+print("Loaded custom gesture map: ", loaded_custom_gesture_map)
+"""
